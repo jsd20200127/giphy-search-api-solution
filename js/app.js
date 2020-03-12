e14a4a53f79b298440dff5a754683bbaf229241d
Kareem Grant <kareem@getuserwise.com>
2020-03-12 09:15:13 -0400

    update solution

--- js/app.js
+++ js/app.js
@@ -1,48 +1,57 @@
 $(function () {
-  $('#search').submit((event) => {
+  $("#search").submit((event) => {
     event.preventDefault()
-    console.log('form being submitted')
+    console.log('submitting form')
+    const input = $("#query").val()
+    console.log(input)
 
-    const query = $('#query').val()
-    console.log(query)
-
-    $('#results-table tbody').html('')
-    $('#query').val('')
-
-    search(query)
+    search(input)
   })
 
-  function displayResults (gifs) {
-    console.log(gifs)
-    gifs.forEach((gif) => {
-      $('#results-table tbody').append(
-        `<tr>
-          <td>${gif.title}</td>
-          <td><img src="${gif.images.fixed_height.url}"></td>
-          <td>${gif.rating}</td>
-          <td><a href="${gif.url}"> link </a></td>
-        </tr>`
-      )
-    })
-  }
-
-  function search (searchTerm) {
-    const url = 'https://api.giphy.com/v1/gifs/search'
+  function search(query) {
+    const url = "https://api.giphy.com/v1/gifs/search"
     const apiKey = 'ADD_YOUR_API_KEY_HERE'
 
     $.ajax({
       url: url,
-      type: 'GET',
-      data: { q: searchTerm, limit: 50, api_key: apiKey }
+      type: "GET",
+      data: { api_key: apiKey, q: query, limit: 50 }
     })
     .done((response) => {
       // execute this function if request is successful
-      console.log(response)
+      console.log(response.data)
+
+      // pass array of gifs as a parameter from API to displayResult() function
       displayResults(response.data)
     })
     .fail(() => {
       // execute this function if request fails
-      console.log('error occurred')
+      alert('error occurred')
     })
   }
+
+  function displayResults(gifs) {
+    // gifs = [{}, {}, {}]
+
+    const gifRows = gifs.map((gif) => {
+      // generate an html row (<tr>) for each gif in our array
+      return (
+        `
+        <tr>
+          <td>${gif.title}</td>
+          <td>
+            <img src="${gif.images.fixed_width_small.url}" />
+          </td>
+          <td>${gif.rating}</td>
+          <td><a href="${gif.url}" target="_blank">${gif.url}</a></td>
+        </tr>
+        `
+      )
+    })
+
+    console.log(gifRows)
+
+    $('tbody').html(gifRows)
+  }
+
 })
